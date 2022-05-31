8771148f8ab49c34b0c02164f13561bed6b4b387
Artis Vansovics <vansovics.artis@gmail.com>
2022-05-31 14:03:44 +0300

    Imported cssReset.scss, base.scss and flexboxgrid.scss to maint.ts. Moved App.vue styles to App.module.scss.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import "./styles/cssReset.scss";
+import "./styles/base.scss";
+import "./styles/flexboxgrid.scss";
 
 createApp(App).use(router).mount("#app");
